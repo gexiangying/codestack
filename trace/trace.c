2e491ff5b5a04d10a43f578cfa9e1b17c60d42e7
gexiangying <19727709@qq.com>
2016-01-29 21:54:08 +0800

    add trace

---
+++ trace/trace.c
@@ -0,0 +1,27 @@
+#define TRACE_DLL
+#include "trace.h"
+#include <windows.h>
+#include <stdio.h>
+#include <stdarg.h>
+int TraceInit()
+{
+	return AllocConsole();
+}
+int TraceClose()
+{
+	return FreeConsole();
+}
+int TraceOut(char* format,...)
+{
+	char msg[1024];
+	unsigned long count = 0;
+	HANDLE hStdOut=GetStdHandle(STD_OUTPUT_HANDLE);
+	if(hStdOut == INVALID_HANDLE_VALUE )
+		return 0;
+	va_list argList;
+  va_start(argList, format);
+  vsprintf(msg,format, argList);
+  va_end(argList);
+	WriteFile(hStdOut,msg,strlen(msg),&count,NULL);
+	return count;
+}
