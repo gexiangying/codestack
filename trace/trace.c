170f37b17470ac10e0dbfbb7f3dd2ec6cf8fafa4
gexiangying <19727709@qq.com>
2017-04-03 19:45:47 +0800

    fix TraceOut _vscprintf

--- trace/trace.c
+++ trace/trace.c
@@ -13,15 +13,19 @@ int TraceClose()
 }
 int TraceOut(char* format,...)
 {
-	char msg[1024] = {0};
 	unsigned long count = 0;
 	HANDLE hStdOut=GetStdHandle(STD_OUTPUT_HANDLE);
 	if(hStdOut == INVALID_HANDLE_VALUE )
 		return 0;
 	va_list argList;
   va_start(argList, format);
-  _vsnprintf(msg,1023,format,argList);
-  va_end(argList);
-	WriteFile(hStdOut,msg,strlen(msg),&count,NULL);
+	int size = _vscprintf(format,argList);
+	char* msg = calloc(size + 1,sizeof(char));
+	if(msg != NULL){
+		_vsnprintf(msg,size + 1,format,argList);
+		va_end(argList);
+		WriteFile(hStdOut,msg,size + 1,&count,NULL);
+		free(msg);
+	}
 	return count;
 }
